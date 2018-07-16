b84f320811ea673aed22a10bffb3b29589c759f4
hawaiianmoose <baclifton@gmail.com>
2018-07-16 00:10:18 -0400

    cloud sucks

--- app/src/main/java/utils/WeatherToIconConverter.kt
+++ app/src/main/java/utils/WeatherToIconConverter.kt
@@ -13,9 +13,6 @@ object WeatherToIconConverter {
         if (lowercaseWeatherText.contains("sun") || lowercaseWeatherText.contains("clear")) {
             return R.drawable.ic_weather_sunny
         }
-        if (lowercaseWeatherText.contains("cloud")) {
-            return R.drawable.ic_weather_cloudy
-        }
         if (lowercaseWeatherText.contains("snow")) {
             return R.drawable.ic_weather_snowy
         }
@@ -30,6 +27,9 @@ object WeatherToIconConverter {
         }
         if (lowercaseWeatherText.contains("mix")) {
             return R.drawable.ic_weather_snowy_rainy
+        }
+        if (lowercaseWeatherText.contains("cloud")) {
+            return R.drawable.ic_weather_cloudy
         }
 
         return R.drawable.ic_weather_partlycloudy
