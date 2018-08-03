0fa6d46ddd6114de0a2d42165adb32e04e7c4983
hawaiianmoose <baclifton@gmail.com>
2018-08-03 16:30:21 -0400

    adding logic for email and rate

--- app/src/main/java/hawaiianmoose/gnarlift/InfoFragment.kt
+++ app/src/main/java/hawaiianmoose/gnarlift/InfoFragment.kt
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_info.*
+import android.content.Intent
+import android.net.Uri
+import android.content.ActivityNotFoundException
 
 class InfoFragment : Fragment() {
 
@@ -15,6 +19,26 @@ class InfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        rate_card_view.setOnClickListener{
+            val uri = Uri.parse("market://details?id=" + context?.packageName)
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + context?.packageName)))
+            }
+        }
+
+        request_card_view.setOnClickListener{
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "baclifton@gmail.com", null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "New Resort Request for Gnarlift")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello, I would love to see {RESORT_NAME} in Gnarlift!")
+            startActivity(Intent.createChooser(emailIntent, "Submit resort request email..."))
+        }
     }
 
 }
